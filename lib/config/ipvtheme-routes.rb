9720f9c0a549a5ee96135485d0c19a9c6899ebb5
Seb Bacon <seb.bacon@gmail.com>
2012-07-16 10:30:01 +0100

    Don't use dispatcher method with routes - it won't work.

--- lib/config/ipvtheme-routes.rb
+++ lib/config/ipvtheme-routes.rb
@@ -1,13 +1,9 @@
 # Here you can override or add to the pages in the core website
 
+ActionController::Routing::Routes.draw do |map|
+    map.with_options :controller => 'user' do |user|
+        user.signchangeaddress '/profile/change_address',      :action => 'signchangeaddress'
+        user.signchangedob '/profile/change_dob',      :action => 'signchangedob'
+    end    
 
-require 'dispatcher'
-Dispatcher.to_prepare do
-    ActionController::Routing::Routes.draw do |map|
-        map.with_options :controller => 'user' do |user|
-            user.signchangeaddress '/profile/change_address',      :action => 'signchangeaddress'
-            user.signchangedob '/profile/change_dob',      :action => 'signchangedob'
-        end    
-
-    end
 end
