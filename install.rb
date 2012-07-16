21239eccb0b9e4069718f7991503979f2cb599f0
Seb Bacon <seb.bacon@gmail.com>
2012-07-16 11:11:44 +0100

    Trying out requiring commands/runner to fix dependency loading problem

--- install.rb
+++ install.rb
@@ -17,7 +17,19 @@ else # Create symlink
 end
 
 # migration-type stuff
+# DOB and address for users
+def column_exists?(table, column)
+    # XXX ActiveRecord 3 includes "column_exists?" method on `connection`
+    return ActiveRecord::Base.connection.columns(table.to_sym).collect{|c| c.name.to_sym}.include? column
+end
+
+if !column_exists?(:users, :dob) || !column_exists?(:users, :address)
+    require File.expand_path '../db/migrate/ipvtheme_add_address_and_dob_to_user', __FILE__
+    IpvthemeAddAddressAndDobToUser.up
+end
+
 # if not already created, make a CensorRule that hides personal information
+require 'commands/runner'
 
 regexp = '([^=]*)={8,}.*\n(?:.*?#.*?: ?.*\n){3,}.*={8,}'
 rule = CensorRule.find_by_text(regexp)
@@ -28,16 +40,5 @@ if rule.nil?
                       :regexp => true,
                       :last_edit_editor => 'system',
                       :last_edit_comment => 'Added automatically by ipvtheme')
-end
-
-# DOB and address for users
-def column_exists?(table, column)
-    # XXX ActiveRecord 3 includes "column_exists?" method on `connection`
-    return ActiveRecord::Base.connection.columns(table.to_sym).collect{|c| c.name.to_sym}.include? column
-end
-
-if !column_exists?(:users, :dob) || !column_exists?(:users, :address)
-    require File.expand_path '../db/migrate/ipvtheme_add_address_and_dob_to_user', __FILE__
-    IpvthemeAddAddressAndDobToUser.up
 end
 
