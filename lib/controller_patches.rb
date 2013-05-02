168379ac4b94b2350fb4df959d17e6b64e8b2616
Louise Crow <louise.crow@gmail.com>
2013-05-02 15:42:55 +0100

    Dispatcher.to_prepare => Rails.configuration.to_prepare

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -4,8 +4,7 @@
 # classes are reloaded, but initialization is not run each time.
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
-require 'dispatcher'
-Dispatcher.to_prepare do
+Rails.configuration.to_prepare do
 
 
     AdminRequestController.class_eval do
