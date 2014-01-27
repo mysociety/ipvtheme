988013dede34419016473c655a913e933c5e1f85
Louise Crow <louise.crow@gmail.com>
2014-01-27 14:24:43 +0000

    Re-add install/uninstall hooks
    Cherry-pick of 476232f: Revert "No need to symlink anymore."
    
    This reverts commit 236716a0e37322a5563d9a0e6aea386e6f4e9827.
    The symlink is required for the new donation microsite.

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,9 @@
+# -*- encoding : utf-8 -*-
 # Uninstall hook code here
+
+main_app_path = Rails.root.join('public', 'alavetelitheme')
+if File.exists?(main_app_path) && File.symlink?(main_app_path)
+  print "Deleting symbolic link at #{main_app_path}... "
+  File.delete(main_app_path)
+  puts "done"
+end
