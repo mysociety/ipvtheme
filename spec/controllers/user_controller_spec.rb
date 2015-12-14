1e3464229cdca6255ea803c87d9b6092f9373186
Louise Crow <louise.crow@gmail.com>
2015-12-14 17:19:23 +0000

    Add specs for the date of birth change action.

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -53,4 +53,74 @@ describe UserController do
 
   end
 
+  describe 'GET signchangedob' do
+
+    render_views
+
+
+    it "redirects an anonymous request to the signin page" do
+      get :signchangedob
+      expect(response).to redirect_to(signin_path(:token => PostRedirect.last.token))
+    end
+
+    it "displays the dob template" do
+      user = FactoryGirl.create(:user, :dob => '2/2/2000')
+      session[:user_id] = user.id
+      get :signchangedob
+      expect(response).to render_template('signchangedob')
+    end
+
+    context 'when supplied with the  "submitted_signchangedob_do" param' do
+
+        it "sets the user's dob" do
+          user = FactoryGirl.create(:user, :dob => '2/2/2000')
+          session[:user_id] = user.id
+          get :signchangedob, {:submitted_signchangedob_do => 1,
+                               :signchangedob =>
+                                 {:dob => '1/1/1900' }}
+          expect(User.where(:id => user.id).first.dob).to eq(Date.new(1900, 1, 1))
+        end
+
+      context 'when the dob param is valid' do
+
+          it 'shows a notice' do
+            user = FactoryGirl.create(:user, :dob => '2/2/2000')
+            session[:user_id] = user.id
+            get :signchangedob, {:submitted_signchangedob_do => 1,
+                                 :signchangedob =>
+                                   {:dob => '1/1/1900' }}
+            message = 'You have now changed your dob used on Alaveteli'
+            expect(flash[:notice]).to eq(message)
+          end
+
+          it "redirects to the user's page" do
+            user = FactoryGirl.create(:user, :dob => '2/2/2000')
+            session[:user_id] = user.id
+            get :signchangedob, {:submitted_signchangedob_do => 1,
+                                 :signchangedob =>
+                                   {:dob => '1/1/1900' }}
+            expected_url = show_user_path(:url_name => user.url_name)
+            expect(response).to redirect_to(expected_url)
+          end
+
+        end
+
+        context 'when the dob param is not valid' do
+
+
+          it "displays the dob template" do
+            user = FactoryGirl.create(:user, :dob => '2/2/2000')
+            session[:user_id] = user.id
+            get :signchangedob, {:submitted_signchangedob_do => 1,
+                                 :signchangedob =>
+                                   {:dob => 'notadate' }}
+            expect(response).to render_template('signchangedob')
+          end
+
+        end
+
+    end
+
+  end
+
 end
